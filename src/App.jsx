00d1bc7e8ce0284e1ac1d777aa2b9cb29d24194e
Wassim Mansoury <wassimelmansoury@gmail.com>
2026-03-05 10:41:53 +0000

    Ajout de la section Header avec le profil utilisateur et les notifications

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import TournamentCard from "./components/TournamentCard";
+
+function App(){
+  return (
+    <>
+    <div className="Home-Page"></div>
+    <header className="header">
+      <div>
+        <h2>Good Morning</h2>
+        <p>Samuel walker !</p>
+      </div>
+      <div className="header-icons">
+        <img src="/images/notification-bell.png" alt="Notification" />
+        <img src="/images/user.png" alt="user"/>
+      </div>
+    </header>
+      </>
+);
+}
