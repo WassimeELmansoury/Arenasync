e2621671d3172a656bf3ba59551705eda6cae6b4
Wassim Mansoury <wassimelmansoury@gmail.com>
2026-03-09 10:18:33 +0000

    ajouter le header de la page tournoi avec bouton retour

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -9,7 +9,16 @@ function SecondPage() {
         return <div>Tournament not found!</div>
     }
     return(
-
+        <>
+        <div className="second-page">
+            <header className="tournament-header">
+                <button onClick={() => navigate(-1)} className="back-btn">
+                    <i className="fa-solid fa-chevron-left"></i>
+                </button>
+                <h2 className="tournament-title">Tournament</h2>
+            </header>
+         </div>
+        </>
     )
 }
 export default SecondPage;
