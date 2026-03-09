aaea99851219d676acc087323cca70939e090ffa
Wassim Mansoury <wassimelmansoury@gmail.com>
2026-03-09 10:17:45 +0000

    Il récupère l’ID

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -1,10 +1,15 @@
+import { useParams, useNavigate } from "react-router-dom"; 
+import { tournamentData } from "../data/tournamentDB";
+
 function SecondPage() {
+    const {id}=useParams();
+    const navigate = useNavigate();
+    const tournament = tournamentData.find((t) => t.id === id);
+    if (!tournament){
+        return <div>Tournament not found!</div>
+    }
     return(
-        <>
-        <div>
-            
-        </div>
-        </>
+
     )
 }
 export default SecondPage;
