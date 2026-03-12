f8c00e63d20abe8300053a21cf05ff242309f981
Wassim Mansoury <wassimelmansoury@gmail.com>
2026-03-12 15:30:49 +0000

    mitre a joure sur la forme de signUp

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,9 +1,17 @@
+import { Activity, useState } from "react";
 function SignupForm(){
+    let [avatar,setAvatar]=useState("")
     return(<>
     <div>
         <h1>S'inscrire maintenant</h1>
         <div>
-            <input type="file" accept="image/*"/>
+            <input type="file" accept="image/*" onChange={(ev)=>{
+                const file=ev.target.files[0]
+                if (file){
+                    setAvatar(URL.createObjectURL(file))
+                }
+            }}/>
+            <img src={avatar} width="50px" height="50px"/>
         </div>
     </div>
     </>
