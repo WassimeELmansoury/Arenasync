6cd8d2b87ae5864d20c7114165e62dc3f427e2d4
Wassim Mansoury <wassimelmansoury@gmail.com>
2026-03-13 15:32:41 +0000

    Edit

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,8 +1,23 @@
 import { useState } from "react";
+import { tournamentData } from "../data/tournamentDB";
 
-function SignupForm() {
-  const [avatar, setAvatar] = useState("");
-
+function SignupForm({tournmentid}) {
+  const [formValue,SetFormValue]=useState({
+    id:"W20",
+    name:"",
+    status:"",
+    avatar:"",
+  })
+  function AddUser(){
+    if( (formValue.name=="") || (formValue.status=="") || (formValue.avatar=="")){
+            alert("remplir tout les champs")
+    }else{
+      let tourn=tournamentData.find((it)=>{it.id==tournmentid})
+      if(tourn!=undefined){
+        tourn.participants.push(formValue)
+      }
+    }
+  }
   return (
     <div className="flex justify-center mt-10">
       <div className="bg-white shadow-xl rounded-2xl p-6 w-[350px]">
@@ -14,20 +29,17 @@ function SignupForm() {
         {/* Avatar */}
         <div className="flex flex-col items-center gap-3 mb-4">
           <input
-            type="file"
-            accept="image/*"
-            className="text-sm"
-            onChange={(ev) => {
-              const file = ev.target.files[0];
-              if (file) {
-                setAvatar(URL.createObjectURL(file));
+            type="file" accept="image/*" className="text-sm" onChange={(ev) => {
+            const file = ev.target.files[0];
+            if (file) {
+               SetFormValue({...formValue,avatar:URL.createObjectURL(file)})
               }
             }}
           />
 
-          {avatar && (
+          {formValue.avatar && (
             <img
-              src={avatar}
+              src={formValue.avatar}
               alt="avatar"
               className="w-16 h-16 rounded-full object-cover border"
             />
@@ -37,29 +49,23 @@ function SignupForm() {
         {/* Inputs */}
         <div className="flex flex-col gap-3">
           <input
-            type="text"
-            placeholder="Nom"
-            className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-[#524FF4]"
+            type="text" placeholder="Nom" className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-[#524FF4]"
+            onChange={(e)=>{SetFormValue({...formValue,name:e.target.value})}}
           />
 
-          <input
-            type="text"
-            placeholder="Prénom"
-            className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-[#524FF4]"
-          />
 
           <input
-            type="email"
-            placeholder="Email"
-            className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-[#524FF4]"
+            type="text" placeholder="Status" className="border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-[#524FF4]"
+             onChange={(e)=>{SetFormValue({...formValue,status:e.target.value})}}
+            
           />
         </div>
 
         {/* Button */}
-        <button className="mt-5 w-full bg-[#524FF4] text-white font-semibold py-2 rounded-lg hover:opacity-90 transition">
+        <button className="mt-5 w-full bg-[#524FF4] text-white font-semibold py-2 rounded-lg hover:opacity-90 transition" onClick={()=>AddUser()}>
           Valider l'inscription
         </button>
-        <button     className="w-full bg-gray-200 text-gray-700 font-semibold py-2 rounded-lg hover:bg-gray-300 transition">
+        <button className="w-full bg-gray-200 text-gray-700 font-semibold py-2 rounded-lg hover:bg-gray-300 transition">
           Cancel
         </button>
       </div>
